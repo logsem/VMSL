1eee6df14c67aa97b556bf60e7c121029739fc54
Kaptch <kaptch@gmail.com>
2021-05-18 21:25:56 +0200

    missing files

---
+++ src/theories/machine.v
@@ -0,0 +1,249 @@
+From stdpp Require Import countable fin vector.
+From Coq Require Import ssreflect Bool Eqdep_dec.
+From ExtLib Require Import Structures.Monads.
+
+Open Scope general_if_scope.
+
+Definition reg_count_lower_bound := 30.
+Definition word_size_lower_bound := 15.
+Definition page_size_lower_bound := 15.
+
+(* TODO: ZArith seems to be much more convenient *)
+Class MachineParameters := {
+  word_size : nat;
+  word_size_at_least : word_size_lower_bound < word_size;
+  
+  reg_count : nat;
+  reg_count_at_least : reg_count_lower_bound < reg_count;
+  
+  page_size : nat; (* in words *)
+  page_size_at_least : page_size_lower_bound < page_size;
+  
+  page_count : nat;
+  
+  decode_pid : fin word_size -> option (fin page_count);
+  encode_pid : fin page_count -> fin word_size;
+  decode_encode_pid : forall (pid : fin page_count),
+      decode_pid (encode_pid pid) = Some pid;
+  
+  page_size_sanity : page_size * page_count = word_size;
+  
+  mm_translation : fin word_size -> fin page_count;
+  mm_translation_inv : fin page_count -> vec (fin word_size) page_size;
+  mm_translation_mm_translation_inv : forall (addr : fin word_size),
+      In addr (mm_translation_inv (mm_translation addr));
+  mm_translation_inv_mm_translation : forall (pid : fin page_count),
+      Forall (fun x => x = pid) (map mm_translation (mm_translation_inv pid));
+  }.
+
+Context `(MachineParams : MachineParameters).
+
+Definition word : Type :=
+  fin word_size.
+
+Instance eq_decision_word : EqDecision word.
+Proof.
+  solve_decision.
+Qed.
+
+Instance countable_word : Countable word.
+Proof.
+  refine {| encode := _;
+            decode := _;
+            decode_encode := _ |}.
+  apply fin_countable.
+Qed.
+
+Inductive reg_name : Type :=
+| PC
+| NZ
+| R (n : nat) (fin : n < reg_count).
+
+Instance eq_decision_reg_name : EqDecision reg_name.
+Proof.
+  intros x y. destruct x as [| | n fin], y as [| | n' fin']; try (by left); try (by right).
+  destruct (nat_eq_dec n n').
+  - subst n'; left; f_equal; apply proof_irrel.
+  - right; congruence.
+Qed.
+
+Program Definition n_to_regname (n : nat) : option reg_name :=
+  if (nat_lt_dec n reg_count) then Some (R n _) else None.
+Next Obligation.
+  auto.
+Defined.
+
+Instance countable_reg_name : Countable reg_name.
+Proof.
+  refine {| encode r := encode match r with
+                               | PC => inl false
+                               | NZ => inl true
+                               | R n fin => inr n
+                               end;
+            decode n := match (decode n) with
+                        | Some (inl false) => Some PC
+                        | Some (inl true) => Some NZ
+                        | Some (inr n) => n_to_regname n
+                        | None => None
+                        end;
+            decode_encode := _ |}.
+  intro r. destruct r; auto.
+  rewrite decode_encode.
+  unfold n_to_regname.
+  destruct (nat_lt_dec n reg_count).
+  - do 2 f_equal; apply proof_irrel.
+  - exfalso; auto.
+Qed.
+
+Inductive ownership : Type :=
+| Owned
+| NotOwned.
+
+Instance eq_decision_ownership : EqDecision ownership.
+Proof.
+  solve_decision.
+Qed.
+
+Inductive access : Type :=
+| NoAccess
+| SharedAccess
+| ExclusiveAccess.
+
+Instance eq_decision_access : EqDecision access.
+Proof.
+  solve_decision.
+Qed.
+
+Definition permission : Type :=
+  (ownership * access).
+
+Definition is_accessible (p : permission) : bool :=
+  match p with
+  | (_, SharedAccess) => true
+  | (_, ExclusiveAccess) => true
+  | _ => false
+  end.
+
+Definition is_owned (p : permission) : bool :=
+  match p with
+  | (Owned, _) => true
+  | _ => false
+  end.
+
+Instance eq_decision_permission : EqDecision permission.
+Proof.
+  solve_decision.
+Qed.
+
+Inductive transaction_type : Type :=
+| Donation
+| Sharing
+| Lending.
+
+Instance eq_decision_transaction_type : EqDecision transaction_type.
+Proof.
+  solve_decision.
+Qed.
+
+Inductive instruction : Type :=
+| Mov (dst : reg_name) (src : word + reg_name)
+| Ldr (dst : reg_name) (src : reg_name)
+| Str (src : reg_name) (dst : reg_name)
+| Cmp (arg1 : reg_name) (arg2 : word + reg_name)
+| Jnz (arg : reg_name)
+| Jmp (arg : reg_name)
+| Halt
+| Fail
+| Hvc.
+
+Definition double_word : Type :=
+  (word * word).
+
+Class InstructionSerialization := {
+  decode_instruction : double_word -> option instruction;
+  encode_instruction : instruction -> double_word;
+  decode_encode_instruction : forall (i : instruction), decode_instruction (encode_instruction i) = Some i;
+                                 }.
+
+Context `(InstrSer : InstructionSerialization).
+
+Instance eq_decision_instruction : EqDecision instruction.
+Proof.
+  solve_decision.
+Qed.
+
+Instance countable_instruction : Countable instruction.
+Proof.
+  refine {| encode := fun i => encode (encode_instruction i);
+            decode := fun p => match decode p with
+                               | None => None
+                               | Some p' => decode_instruction p'
+                               end;
+            decode_encode := _ |}.
+  intros x.
+  rewrite decode_encode.
+  apply decode_encode_instruction.
+Qed.
+
+Definition addr : Type := word.
+
+Instance eq_decision_addr : EqDecision addr.
+Proof.
+  solve_decision.
+Qed.
+
+Instance countable_addr : Countable addr.
+Proof.
+  refine {| encode := _;
+            decode := _;
+            decode_encode := _ |}.
+  apply fin_countable.
+Qed.
+
+Inductive hvc_func : Type :=
+  Run
+| Yield
+| Share
+| Lend 
+| Donate
+| Retrieve
+| Relinquish
+| Reclaim
+| Send 
+| Wait.
+
+Inductive hvc_ret_code : Type :=
+  Error
+| Succ.
+
+Inductive hvc_error : Type :=
+  InvParam
+| Denied
+| Busy
+| Ready
+| NoMem.
+
+Class HypervisorParameters := {
+  vm_count : nat;
+  vm_count_pos : 0 < vm_count;
+  decode_vmid : word -> option (fin vm_count);
+  encode_vmid : (fin vm_count)  -> word;
+  decode_encode_vmid : forall (vmid : fin vm_count),
+      decode_vmid (encode_vmid vmid) = Some vmid;
+  decode_hvc_func : word -> option hvc_func;
+  encode_hvc_func : hvc_func -> word;
+  decode_encode_hvc_func : forall (hvc : hvc_func),
+      decode_hvc_func (encode_hvc_func hvc) = Some hvc;
+  decode_hvc_error : word -> option hvc_error;
+  encode_hvc_error : hvc_error -> word;
+  decode_encode_hvc_error : forall (hvc : hvc_error),
+      decode_hvc_error (encode_hvc_error hvc) = Some hvc;
+  decode_hvc_ret_code : word -> option hvc_ret_code;
+  encode_hvc_ret_code : hvc_ret_code -> word;
+  decode_encode_hvc_ret_code : forall (hvc : hvc_ret_code),
+      decode_hvc_ret_code (encode_hvc_ret_code hvc) = Some hvc;
+  decode_transaction_type : word -> option transaction_type;
+  encode_transaction_type : transaction_type -> word;
+  decode_encode_transaction_type : forall (ty : transaction_type),
+      decode_transaction_type (encode_transaction_type ty) = Some ty
+                             }.
---
+++ src/theories/monad.v
@@ -0,0 +1,453 @@
+(* From ExtLib Require Import Structures.Monads. *)
+From stdpp Require Import vector.
+
+Class Functor (F : Type -> Type) := {
+  fmap {A B : Type} (f : A -> B) (fa : F A) : F B;
+  fmap_id {A : Type} : forall x, fmap (@id A) x = id x;
+  fmap_compose {A B C : Type} : forall (g : B -> C) (h : A -> B) x, fmap (compose g h) x = compose (fmap g) (fmap h) x;
+                                   }.
+
+Delimit Scope functor_scope with functor.
+
+Notation "f <$> a" := (@fmap _ _ _ _ f a) (at level 61, left associativity) : functor_scope.
+
+Class Applicative (F : Type -> Type) := {
+  pure {A : Type} (a : A) : F A;
+  ap {A B : Type} (f : F (A -> B)) (a : F A) : F B;
+  ap_pure_id {A : Type} : forall (v : F A),
+      ap (pure id) v = v;
+  pure_composition {A B C : Type} : forall (u : F (B -> C)) (v : F (A -> B)) (w : F A),
+      ap (ap (ap (pure compose) u) v) w = ap u (ap v w);
+  pure_homomorphism {A B : Type} : forall (f : A -> B) (x : A),
+      ap (pure f) (pure x) = pure (f x);
+  pure_interchange {A B : Type} : forall (u : F (A -> B)) (y : A),
+      ap u (pure y) = ap (pure (fun f => f y)) u;
+                                                    }.
+
+Instance applicative_implies_functor {F : Type -> Type} `{Applicative F} : Functor F.
+Proof.
+  refine {|
+      fmap := fun {A B : Type} (f : A -> B) (x : F A) => ap (pure f) x;
+      fmap_compose := _;
+      fmap_id := _;
+    |}.
+  - intros; rewrite ap_pure_id; reflexivity.
+  - intros; simpl; rewrite <- pure_composition; rewrite 2 pure_homomorphism; reflexivity.
+Defined.
+
+Delimit Scope applicative_scope with applicative.
+
+Notation "f <*> a" := (@ap _ _ _ _ f a) (at level 62, left associativity) : applicative_scope.
+
+Class Monad (M : Type -> Type) := {
+  unit {A : Type} (a : A) : M A;
+  bind {A B : Type} : M A -> (A -> M B) -> M B;
+  unit_l_id_bind {A B : Type} : forall (f : A -> M B) (a : A),
+      bind (unit a) f = f a;
+  unit_r_id_bind {A B : Type} : forall (ma : M A),
+      bind ma unit = ma;
+  bind_assoc {A B C : Type} : forall (f : A -> M B) (g : B -> M C) (ma : M A),
+      bind ma (fun x => bind (f x) g) = bind (bind ma f) g;
+                                                  }.
+
+Definition join {M : Type -> Type} {A : Type} `{Monad M} (mma : M (M A)) : M A :=
+  bind mma id.
+
+Class Monoid (M : Type) := {
+  op : M -> M -> M;
+  neutral : M;
+  unit_id_l : forall (a : M), op neutral a = a;
+  unit_id_r : forall (a : M), op a neutral = a;
+  op_assoc {a b c : M} : op a (op b c) = op (op a b) c;
+                          }.
+
+Class Foldable (T : Type -> Type) := {
+  foldr {A B : Type} : (A -> B -> B) -> B -> T A -> B;
+  fold_map {A : Type} {M : Type} `{Monoid M} : (A -> M) -> T A -> M;
+                                   }.
+
+Class Traversable (T : Type -> Type) `{Foldable T} `{Functor T} := {
+  traverse {A B : Type} {F : Type -> Type} `{Applicative F} : (A -> F B) -> T A -> F (T B);
+  sequence_a {A : Type} {F : Type -> Type} `{Applicative F} : T (F A) -> F (T A); 
+  }.
+
+Module MonadExt.
+  From Coq Require Import Logic.FunctionalExtensionality.
+
+  Instance monad_implies_applicative {M : Type -> Type} `{Monad M} : Applicative M.
+  Proof.
+    refine {|
+        pure := fun {A : Type} (x : A) => unit x;
+        ap := fun {A B : Type} (f : M (A -> B)) (a : M A) => bind f (fun g => bind a (fun x => unit (g x)));
+      |}.
+    - intros A v.
+      rewrite unit_l_id_bind.
+      simpl.
+      exact (@unit_r_id_bind _ _ A A v).
+    - intros A B C u v w.
+      rewrite unit_l_id_bind.
+      rewrite <- 2 bind_assoc.
+      f_equal.
+      apply functional_extensionality.
+      intros g.
+      rewrite unit_l_id_bind.
+      rewrite <- 2 bind_assoc.
+      f_equal.
+      apply functional_extensionality.
+      intros f.
+      rewrite unit_l_id_bind.
+      rewrite <- bind_assoc.
+      f_equal.
+      apply functional_extensionality.
+      intros a.
+      rewrite unit_l_id_bind.
+      reflexivity.
+    - intros A B f x.
+      rewrite 2 unit_l_id_bind.
+      reflexivity.
+    - intros A B u y.
+      rewrite unit_l_id_bind.
+      f_equal.
+      apply functional_extensionality.
+      intros f.
+      rewrite unit_l_id_bind.
+      reflexivity.
+  Defined.
+End MonadExt.
+
+Module Option.
+
+  Instance functor_option : Functor option.
+  Proof.
+    refine {| fmap := fun {A B : Type} (f : A -> B) (x : option A) => match x with | None => None | Some x' => Some (f x') end; |}.
+    - intros A [x |]; reflexivity.
+    - intros A B C g h [x |]; reflexivity.
+  Defined.
+
+  Instance applicative_option : Applicative option.
+  Proof.
+    refine {| pure := fun {A : Type} (x : A) => Some x;
+              ap := fun {A B : Type} (f : option (A -> B)) (a : option A) =>
+                      match f with
+                      | None => None
+                      | Some f' =>
+                        match a with
+                        | None => None
+                        | Some a' => Some (f' a')
+                        end
+                      end; |}.
+    - intros A [v |]; reflexivity.
+    - intros A B C [u |] [v |] [w |]; reflexivity.
+    - reflexivity.
+    - intros A B [u |] y; reflexivity.
+  Defined.
+  
+  Instance monad_option : Monad option.
+  Proof.
+    refine {| unit t x := Some x;
+              bind t u mt f := match mt with | None => None | Some t => f t end;
+           |}.
+    - reflexivity.
+    - intros ? ? [ma |]; reflexivity.
+    - intros A B C f g [ma |]; reflexivity.
+  Defined.
+
+  Fixpoint list_from_some {A : Type} (l : list (option A)) : list A :=
+    match l with
+    | nil => nil
+    | cons x xs =>
+      match x with
+      | None => list_from_some xs
+      | Some x' => cons x' (list_from_some xs)
+      end
+    end.
+  
+  Definition bool_check_option {A : Type} (b : bool) : option bool :=
+    match b with
+    | false => None
+    | true => Some true
+    end.
+  
+End Option.
+
+Module Sum.
+  
+  Instance functor_sum {E : Type} : Functor (sum E).
+  Proof.
+    refine {| fmap := fun {A B : Type} (f : A -> B) (x : E + A) => match x with
+                                     | inl l => inl l
+                                     | inr r => inr (f r)
+                                 end
+           |}.
+    - intros A [e | a]; reflexivity.
+    - intros A B C g h [e | a]; reflexivity.
+  Defined.
+
+  Instance applicative_sum {E : Type} : Applicative (sum E).
+  Proof.
+    refine {| pure := fun {A : Type} (x : A) => inr x;
+              ap := fun {A B : Type} (f : E + (A -> B)) (x : E + A) =>
+                      match f with
+                      | inl l => inl l
+                      | inr r =>
+                        match x with
+                        | inl l' => inl l'
+                        | inr r' => inr (r r')
+                        end
+                      end
+           |}.
+    - intros A [e | a]; reflexivity.
+    - intros A B C [e1 | u] [e2 | v] [e3 | w]; reflexivity.
+    - reflexivity.
+    - intros ? ? [e | f] y; reflexivity.
+  Defined.
+  
+  Instance monad_sum {E : Type} : Monad (sum E).
+  Proof.
+    refine {| unit t x := inr x;
+              bind t u mt f := match mt with | inl l => inl l | inr r => f r end;
+           |}.
+    - reflexivity.
+    - intros ? ? [? | ?]; reflexivity.
+    - intros ? ? ? f g [? | a]; [| destruct (f a)]; reflexivity.
+  Defined.
+  
+  Definition unpack_option_with_left {A B : Type} (v : option B) (err : A) : sum A B :=
+    match v with
+    | None => inl err
+    | Some v' => inr v'
+    end.
+
+  Definition throw {A B : Type} (err : A) : sum A B := inl err.
+  
+End Sum.
+
+Module List.
+
+  Local Fixpoint list_bind {A B : Type} (l : list A) (f : A -> list B) : list B :=
+    match l with
+    | nil => nil
+    | cons x xs => app (f x) (list_bind xs f)
+    end.
+
+  Instance functor_list : Functor list.
+  Proof.
+    refine {| fmap := map |}.
+    - exact map_id.
+    - symmetry.
+      apply map_map.
+  Defined.
+
+  Local Fixpoint foldr_list {A B : Type} (f : A -> B -> B) (b : B) (l : list A) : B :=
+    match l with
+    | nil => b
+    | cons x xs => f x (foldr_list f b xs)
+    end.
+  
+  Local Fixpoint fold_map_list {A M : Type} `{Monoid M} (f : A -> M) (l : list A) : M :=
+    match l with
+    | nil => neutral
+    | cons x xs => op (f x) (fold_map_list f xs)
+    end.
+  
+  Instance foldable_list : Foldable list.
+  Proof.
+    refine {| foldr := @foldr_list;
+              fold_map := @fold_map_list ;|}.
+  Defined.
+  
+  Local Fixpoint sequence_a_list {A : Type} {F : Type -> Type} `{Applicative F} (l : list (F A)) : F (list A) :=
+    foldr (fun val acc => ap (fmap cons val) acc) (pure nil) l.
+
+  Local Definition traverse_list {A B : Type} {F : Type -> Type} `{Applicative F} (f : A -> F B) : list A -> F (list B) :=
+    compose sequence_a_list (fmap f).    
+  
+  Instance traversable_list : Traversable list.
+  Proof.
+    refine {| traverse := @traverse_list;
+              sequence_a := @sequence_a_list;
+           |}.
+  Defined.
+
+  Local Fixpoint ap_list {A B : Type} (f : list (A -> B)) (x : list A) : list B := 
+    match (f, x) with
+    | (nil, _) => nil
+    | (_,  nil) => nil
+    | (cons f' fs, ls) => app (fmap f' ls) (ap_list fs ls)
+    end.
+
+  (* TODO *)
+  Instance applicative_list : Applicative list.
+  Proof.
+    refine {| pure := fun _ x => [x];
+              ap := @ap_list;
+           |}.
+    - intros A [| x xs]; [reflexivity |].
+      simpl.
+      f_equal.
+      rewrite map_id.
+      exact (app_nil_r _).
+    - admit.
+    - intros ? ? f x; reflexivity.
+    - intros ? ? f.
+      induction f as [| f fs IHf].
+      + reflexivity.
+      + intros y.
+        simpl.
+        rewrite app_nil_r.
+        f_equal.
+        rewrite (IHf y).
+        destruct fs.
+        * reflexivity.
+        * simpl.
+          f_equal.
+          rewrite app_nil_r.
+          reflexivity.
+  Admitted.
+
+  (* TODO *)
+  Instance monad_list : Monad list.
+  Proof.
+    refine {| unit t x := [x];
+              bind t u := list_bind
+           |}.
+    - intros ? ? f a; rewrite <- app_nil_r.
+      reflexivity.
+    - intros ? ? l; induction l as [| x xs IHl].
+      + reflexivity.
+      + simpl.
+        f_equal.
+        assumption.
+    - intros ? ? ? f g ma.
+      induction ma as [| x xs IHl].
+      + reflexivity.
+      + simpl.
+        rewrite IHl.
+        admit.
+  Admitted.
+
+End List.
+
+Module ListNoDup.
+  From stdpp Require Import listset_nodup.    
+
+  Local Definition foldr_listset_nodup {A B : Type} (f : A -> B -> B) (b : B) (l : listset_nodup A) : B :=
+    match l with
+    | ListsetNoDup l' _ => foldr f b l'
+    end.
+  
+  Local Definition fold_map_listset_nodup {A M : Type} `{Monoid M} (f : A -> M) (l : listset_nodup A) : M :=
+    match l with
+    | ListsetNoDup l' _ => fold_map f l'
+    end.
+
+  Instance foldable_listset_nodup : Foldable listset_nodup.
+  Proof.
+    refine {| fold_map := @fold_map_listset_nodup |}.
+    apply @foldr_listset_nodup.
+  Defined.
+
+  (* Seems terribly wrong *)
+  Lemma sequence_m_no_dup {A : Type} {M : Type -> Type} `{Monad M} (l : listset_nodup (M A)) : M (listset_nodup A).
+  Proof.
+    destruct l as [car prf].
+    induction car as [| x xs HQ].
+    - apply unit; apply empty.
+    - eapply bind.
+      + exact x.
+      + intros a.
+        eapply bind.
+        * apply HQ.
+          rewrite NoDup_ListNoDup in prf.
+          rewrite List.NoDup_cons_iff in prf.
+          destruct prf as [prf1 prf2].
+          rewrite <- NoDup_ListNoDup in prf2.
+          exact prf2.
+        * intros l'.
+          apply unit.
+          exact l'.
+    Defined.
+  
+End ListNoDup.
+
+Module Vector.
+
+  Local Fixpoint foldr_vec {A B : Type} {n : nat} (f : A -> B -> B) (b : B) (l : vec A n) : B :=
+    match l with
+    | vnil => b
+    | vcons x xs => f x (foldr_vec f b xs)
+    end.
+  
+  Local Fixpoint fold_map_vec {A M : Type} {n : nat} `{Monoid M} (f : A -> M) (l : vec A n) : M :=
+    match l with
+    | vnil => neutral
+    | vcons x xs => op (f x) (fold_map_vec f xs)
+    end.
+  
+  Instance foldable_vec {n : nat} : Foldable (fun x => vec x n).
+  Proof.
+    refine {| foldr := fun A B f acc v => @foldr_vec A B _ f acc v;
+              fold_map := fun A M _ f v => @fold_map_vec A M _ _ f v ;|}.
+  Defined.
+  
+  Local Fixpoint sequence_a_vec {A : Type} {F : Type -> Type} {n : nat} `{Applicative F} (l : vec (F A) n) : F (vec A n) :=
+    match l with
+    | vnil => pure vnil
+    | @Vector.cons _ x n xs => ap (fmap (fun y => @vcons A y n) x) (sequence_a_vec xs)
+    end.
+
+  Local Definition traverse_vec {A B : Type} {F : Type -> Type} {n : nat} `{Applicative F} (f : A -> F B) : vec A n -> F (vec B n) :=
+    compose sequence_a_vec (vmap f).    
+
+  Instance functor_vec {n : nat} : Functor (fun x => vec x n).
+  Proof.
+    refine {| fmap := fun _ _ f v => vmap f v; |}.
+    - intros.
+      rewrite Vector.map_id.
+      reflexivity.
+    - intros A B C g h [| x xs].
+      + reflexivity.
+      + simpl.
+        f_equal.
+        rewrite Vector.map_map.
+        reflexivity.
+  Defined.
+  
+  Instance traversable_vec {n : nat} : Traversable (fun x => vec x n).
+  Proof.
+    refine {| traverse := fun _ _ _ _ => traverse_vec;
+              sequence_a := fun _ _ _ => sequence_a_vec;
+           |}.
+  Defined.
+  
+End Vector.
+
+Module General.
+  
+  Definition map_m {A B : Type} {T : Type -> Type} {M : Type -> Type} `{Monad M} `{Traversable T}
+             (f : A -> M B) (l : T A) : M (T B) := traverse f l.
+  
+End General.
+
+Module MonadNotationBase.
+  
+  Delimit Scope monad_scope with monad.
+  
+  Notation "c >>= f" := (@bind _ _ _ _ c f) (at level 58, left associativity) : monad_scope.
+  Notation "e1 ;;; e2" := (@bind _ _ _ _ e1%monad (fun _ => e2%monad))%monad
+                                                                      (at level 63, right associativity) : monad_scope.
+  
+End MonadNotationBase.
+
+Module MonadNotation.
+  
+  Export MonadNotationBase.
+  
+  Notation "x <- c1 ;;; c2" := (@bind _ _ _ _ c1 (fun x => c2))
+                                (at level 63, c1 at next level, right associativity) : monad_scope.
+  
+  Notation "' pat <- c1 ;;; c2" :=
+    (@bind _ _ _ _ c1 (fun x => match x with pat => c2 end))
+      (at level 63, pat pattern, c1 at next level, right associativity) : monad_scope.
+  
+End MonadNotation.
