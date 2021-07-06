343771b25e4e28f1860bf180a3e89b90ec38fc86
Kaptch <kaptch@gmail.com>
2021-07-06 16:27:08 +0200

    split rules into separate files

---
+++ theories/rules/donate.v
@@ -0,0 +1,42 @@
+From machine_program_logic.program_logic Require Import machine weakestpre.
+From HypVeri Require Import RAs rule_base lifting rules.rules_base.
+From iris.proofmode Require Import tactics.
+Require Import iris.base_logic.lib.ghost_map.
+Require Import stdpp.fin.
+
+Lemma hvc_mem_donate11 {instr i wi r2 ptx so q sa wf wt} ai r0 r1 j pd :
+  instr = Hvc ->
+  decode_instruction wi = Some(instr) ->
+  decode_hvc_func r0 = Some(Donate) ->
+  (fin_to_nat r1) ≤ page_size ->
+  {[pd; (mm_translation ai)]} ⊆ sa ->
+  pd ∈ so ->
+  <<i>> ∗ PC @@ i ->r ai ∗ ai ->a wi
+  ∗ O@i:={q}[so] ∗ A@i:={1}[sa]
+  ∗ R0 @@ i ->r r0 ∗ R1 @@ i ->r r1 ∗ R2 @@i ->r r2 ∗ TX@ i := ptx
+  ∗ new_transaction_descriptor11 ptx i wf zero_word wt one_word j pd
+    ⊢ SSWP ExecI @ i {{ (λ m, ⌜m = HaltI ⌝ ∗ <<i>> ∗ PC @@ i ->r (ai +w 1) ∗ ai ->a wi
+  ∗ O@i:={q}[so] ∗ A@i:={1}[sa∖{[pd]}]
+  ∗ R0 @@ i ->r (encode_hvc_ret_code Succ) ∗ R1 @@ i ->r r1 ∗ TX@ i := ptx
+  ∗ ∃wh, (R2 @@ i ->r wh ∗ wh ->t{1}(i,wf, wt, {[j := {[pd]}]},Donation) ∗ wh ->re j)
+  ∗ new_transaction_descriptor11 ptx i zero_word zero_word zero_word one_word j pd)}}%I.
+Proof.
+  iIntros (Hinstr Hdecodei Hdecodef Hvalidlen Hsa Hso) "(Htok & HPC & Hai & Hown & Haccess & HR0 & HR1 & HR2 & HTX & Htd)".
+  iApply (sswp_lift_atomic_step ExecI);[done|].
+  iIntros (σ1) "%Hsche Hσ".
+  inversion Hsche as [ Hcureq ]; clear Hsche.
+  apply fin_to_nat_inj in Hcureq.
+  iModIntro.
+  iDestruct "Hσ" as "(Hcur & Hmem & Hreg & Htxpage & ? & Hownedpt & Haccesspt & Htrans & Hrcv)".
+  (* valid regs *)
+  iDestruct ((gen_reg_valid4 σ1 i PC ai R0 r0 R1 r1 R2 r2 Hcureq ) with "Hreg HPC HR0 HR1 HR2 ")
+    as "[%HPC [%HR0 [%HR1 %HR2]]]";eauto.
+  (* valid pt *)
+  iDestruct ((gen_access_valid2 σ1 i 1 sa pd (mm_translation ai)) with "Haccesspt Haccess") as %[Haccpd Haccai];eauto.
+  (* valid mem *)
+  iDestruct (gen_mem_valid σ1 ai wi with "Hmem Hai") as %Hmem.
+  iDestruct (gen_mem_valid_td11 σ1 _ _ _ _ _ _ with "Htd Hmem") as %Htd.
+  iSplit.
+  - admit.
+  - admit.
+ Admitted.
---
+++ theories/rules/rules_base.v
@@ -0,0 +1,119 @@
+From machine_program_logic.program_logic Require Import machine weakestpre.
+From HypVeri Require Import RAs rule_base lifting.
+From iris.proofmode Require Import tactics.
+Require Import iris.base_logic.lib.ghost_map.
+Require Import stdpp.fin.
+
+Global Instance hyp_irisG `{gen_VMG Σ}: irisG hyp_machine Σ:=
+  {
+  iris_invG := gen_invG;
+  state_interp := gen_vm_interp
+  }.
+
+Context `{vmG: !gen_VMG Σ}.
+Implicit Type a : addr.
+Implicit Type i : vmid.
+Implicit Type ra rb : reg_name.
+Implicit Type w: word.
+Implicit Type q : Qp.
+
+
+Ltac rewrite_reg_all :=
+  match goal with
+  | |- _ =>
+    try rewrite -> update_offset_PC_preserve_current_vm; try rewrite -> update_reg_global_preserve_current_vm;
+    try rewrite -> update_offset_PC_preserve_mem ; try rewrite -> update_reg_global_preserve_mem;
+    try rewrite -> update_offset_PC_preserve_tx ; try rewrite -> update_reg_global_preserve_tx;
+    try rewrite -> update_offset_PC_preserve_rx ; try rewrite  -> update_reg_global_preserve_rx;
+    try rewrite -> update_offset_PC_preserve_owned  ; try rewrite -> update_reg_global_preserve_owned;
+    try rewrite -> update_offset_PC_preserve_access  ; try rewrite -> update_reg_global_preserve_access;
+    try rewrite -> update_offset_PC_preserve_trans  ; try rewrite -> update_reg_global_preserve_trans;
+    try rewrite -> update_offset_PC_preserve_receivers  ; try rewrite -> update_reg_global_preserve_receivers
+  end.
+
+
+Ltac rewrite_mem_all :=
+  match goal with
+  | |- _ =>
+    try rewrite -> update_memory_unsafe_preserve_current_vm;
+    try rewrite -> update_reg_global_preserve_mem;
+    try rewrite -> update_memory_unsafe_preserve_tx;
+    try rewrite -> update_memory_unsafe_preserve_rx;
+    try rewrite -> update_memory_unsafe_preserve_owned;
+    try rewrite -> update_memory_unsafe_preserve_access;
+    try rewrite -> update_memory_unsafe_preserve_trans;
+    try rewrite -> update_memory_unsafe_preserve_receivers
+  end.
+
+Ltac solve_reg_lookup :=
+  match goal with
+  | _ : get_reg ?σ ?r = Some ?w |- get_reg_gmap ?σ !! (?r, ?i) = Some ?w => rewrite get_reg_gmap_get_reg_Some;eauto
+  | _ : get_reg ?σ ?r = Some ?w |- is_Some (get_reg_gmap ?σ !! (?r, ?i)) => eexists;rewrite get_reg_gmap_get_reg_Some;eauto
+  | _ : get_reg ?σ ?r1 = Some ?w, _ : ?r1 ≠ ?r2 |- <[(?r2, ?i):= ?w2]>(get_reg_gmap ?σ) !! (?r1, ?i) = Some ?w =>
+    rewrite lookup_insert_ne; eauto
+  end.
+  
+
+Lemma check_access_page_mem_eq {σ i a} :
+  check_access_page σ i (mm_translation a) =
+  check_access_addr σ i a.
+Proof.
+  rewrite /check_access_addr; done.
+Qed.
+
+    
+Lemma not_valid_pc {a i s} :
+  (mm_translation a) ∉ s ->
+  PC @@ i ->r a ∗ A@i:={1}[s]
+  ⊢ SSWP ExecI @ i {{ (λ m, ⌜m = FailI⌝ ∗ PC @@ i ->r a ∗ A@i:={1}[s]) }}%I.
+Proof.
+  iIntros (Hmm) "(Hpc & Ha)".
+  iApply (sswp_lift_atomic_step ExecI);[done|].
+  iIntros (σ1) "%Hsche Hσ1".
+  inversion Hsche as [ Hcur ]; clear Hsche.
+  apply fin_to_nat_inj in Hcur.
+  iModIntro.
+  iDestruct "Hσ1" as "(? & ? & Hreg & ? & ? & ? & Haccess & ?)".
+  iDestruct (gen_reg_valid1 σ1 PC i a Hcur with "Hreg Hpc") as "%Hpc".
+  iDestruct (gen_no_access_valid σ1 i (mm_translation a) s Hmm with "Haccess Ha") as "%Hnacc".
+  iSplit.
+  - iPureIntro.
+    rewrite /reducible.
+    exists FailI, σ1.
+    apply step_exec_fail.
+    rewrite /is_valid_PC Hpc Hcur.
+    simpl.
+    rewrite check_access_page_mem_eq in Hnacc.
+    rewrite Hnacc.
+    done.
+  - iModIntro.
+    iIntros (m2 σ2) "%HstepP".
+    iModIntro.
+    inversion HstepP; subst.
+    + simpl.
+      rewrite /gen_vm_interp.
+      iFrame.
+      iPureIntro.
+      done.
+    + simplify_eq.
+      rewrite /is_valid_PC Hpc in H.      
+      simpl in H.
+      rewrite check_access_page_mem_eq in Hnacc.
+      rewrite Hnacc in H.
+      inversion H.
+Qed.
+
+Lemma eliminate_wrong_token {i j} :
+  j ≠ i ->
+  <<j>> ⊢ SSWP ExecI @ i {{ (λ m, False) }}%I.
+Proof.
+  iIntros (Hne) "Htok".
+  iApply (sswp_lift_atomic_step ExecI) ;[done|].
+  iIntros (σ1) "%Hsche Hσ".
+  iDestruct "Hσ" as "(Htokown & ? & ? & ? & ? & ? & ? & ?)".
+  iDestruct (gen_token_valid_neq j i Hne with "Htok Htokown") as "%Hnsch".
+  iExFalso.
+  iPureIntro.
+  done.
+Qed.
+
