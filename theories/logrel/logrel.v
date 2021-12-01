f18274ead8f55266c3f4f5948b22e34bc753822e
Zongyuan Liu <zy.liu@cs.au.dk>
2021-12-01 17:03:40 +0100

    logrel: definition WIP

--- theories/logrel/logrel.v
+++ theories/logrel/logrel.v
@@ -2,7 +2,6 @@ From iris.proofmode Require Import tactics.
 From machine_program_logic.program_logic Require Import weakestpre.
 From HypVeri.lang Require Import lang.
 From HypVeri.algebra Require Import base.
-From iris.base_logic Require Export invariants na_invariants saved_prop.
 From HypVeri.rules Require Import rules_base.
 Import uPred.
 
@@ -13,13 +12,67 @@ Section logrel.
   Context `{hypparams:!HypervisorParameters}.
   Context `{vmG: !gen_VMG Σ}.
 
-  Notation V := ((leibnizO VMID) -n> page_table -n> iPropO Σ).
+  Notation V := ((leibnizO VMID) -n> (leibnizO page_table) -n> (leibnizO reg_file) -n> iPropO Σ).
   Notation E := ((leibnizO VMID) -n> iPropO Σ).
   Implicit Types i : (leibnizO VMID).
   Implicit Types interp_expr : (E).
 
+  Program Definition V0 : VMID := (@nat_to_fin 0 _ _).
+  Next Obligation.
+  destruct hypconst. simpl. lia.
+  Defined.
+
+  Definition unknown_mem_page (p: PID):=
+   ([∗ list] a ∈ (finz.seq (of_pid p) (Z.to_nat page_size)), ∃ w, (a ->a w))%I.
+
+
+  (** definition **)
   Program Definition interp_execute: E :=
    λne (i: leibnizO VMID), (⌜ fin_to_nat i ≠ 0 ⌝ -∗ (VMProp_holds i (1/2)%Qp -∗ WP ExecI @ i {{(λ _, True )}}))%I.
 
 
-  End logrel.
+  Definition shared_or_noaccess_pages (i:VMID) (pgt: page_table) : iProp Σ:=
+    (
+      [∗ map] p ↦ perm ∈ pgt, let sacc := perm.2 in
+                              ∃ (j: VMID), ⌜j ≠ i ∧ j ∈ sacc⌝ -∗
+                                p -@{1}A> [sacc] ∗
+                                  (⌜i ∈ sacc⌝ -∗ unknown_mem_page p)
+    )%I.
+
+  Definition exclusive_access_pages (i:VMID) (pgt: page_table) : iProp Σ:=
+    (
+      [∗ map] p ↦ perm ∈ pgt, let sacc := perm.2 in
+                               ⌜{[i]} = sacc⌝ -∗
+                                p -@EA> i ∗ unknown_mem_page p
+    )%I.
+
+  Program Definition interp_access: V :=
+    λne (i:leibnizO VMID) (pgt: page_table) (regs: reg_file),
+      ( (* registers *)
+        ([∗ map] r ↦ w ∈ regs, r @@i ->r w) ∗
+        (* VMProp  *)
+        VMProp i (
+          (* in case of yielding, we need the following to apply yield rule*)
+          (* R0 & R1 of pvm *)
+          (R0 @@ V0 ->r encode_hvc_func(Run) ∗ R1 @@ V0 ->r encode_vmid(i) ∗
+          (* VMProp of pvm *)
+          (* if i yielding, we give following resources back to pvm *)
+          VMProp V0 (
+            (* R0 and R1 of pvm *)
+            R0 @@ V0 ->r encode_hvc_func(Yield) ∗ R1 @@ V0 ->r encode_vmid(i) ∗
+            (* exclusive access pagetable entries + mem *)
+            (exclusive_access_pages i pgt) ∗
+            (* shared/noaccess pagetable entries + shared mem *)
+            (shared_or_noaccess_pages i pgt)
+              (* TODO: recursive definition *)
+          ) (1/2)%Qp ∗
+          (* exclusive access pagetable entries + mem *)
+          (exclusive_access_pages i pgt) ∗
+          (* shared/noaccess pagetable entries + shared mem *)
+          (shared_or_noaccess_pages i pgt))
+          (* no scheduling, we finish the proof *)
+          ∨ False
+        ) (1/2)%Qp
+      )%I.
+
+End logrel.
