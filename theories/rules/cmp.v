51f856527ac37bdc35728f5e6d056b35ff51f58d
Kaptch <kaptch@gmail.com>
2021-12-16 16:49:42 +0100

    cmp upd

--- theories/rules/cmp.v
+++ theories/rules/cmp.v
@@ -8,23 +8,27 @@ Section cmp.
 Context `{hypparams: HypervisorParameters}.
 Context `{vmG: !gen_VMG Σ}.
   
-Lemma cmp_word {i w1 w2 w3 w4 q sacc} ai ra :
+Lemma cmp_word {i w1 w2 w3 w4 q s} ai ra :
   decode_instruction w1 = Some(Cmp ra (inl w2)) ->
-  to_pid_aligned ai ∈ sacc ->
+  i ∈ s ->
   {SS{{ ▷ (PC @@ i ->r ai) ∗ ▷ (ai ->a w1) ∗
-        ▷ (ra @@ i ->r w3) ∗ ▷ (A@i:={q}[sacc]) ∗ ▷ (NZ @@ i ->r w4)}}}
+        ▷ (ra @@ i ->r w3) ∗ ▷ ((tpa ai) -@{q}A> [s]) ∗ ▷ (NZ @@ i ->r w4)}}}
     ExecI @ i
-    {{{ RET ExecI; PC @@ i ->r (ai ^+ 1)%f ∗ ai ->a w1 ∗ ra @@ i ->r w3 ∗
-        A@i:={q}[sacc] ∗ NZ @@ i ->r (if (w3 <? (of_imm w2))%f then W2 else if ((of_imm w2) <? w3)%f then W0 else W1) }}}.
+    {{{ RET (false, ExecI); PC @@ i ->r (ai ^+ 1)%f ∗ ai ->a w1 ∗ ra @@ i ->r w3 ∗
+        ((tpa ai) -@{q}A> [s]) ∗ NZ @@ i ->r (if (w3 <? (of_imm w2))%f then W2 else if ((of_imm w2) <? w3)%f then W0 else W1) }}}.
 Proof.
   iIntros (Hdecode Hin ϕ) "(>Hpc & >Hapc & >Hra & >Hacc & >Hnz ) Hϕ".
   iApply (sswp_lift_atomic_step ExecI);[done|].
   set (instr:= Cmp ra (inl w2)).
-  iIntros (σ1) "%Hsche Hσ".
-  inversion Hsche as [ Hcur ]; clear Hsche.
+  iIntros (n σ1) "%Hsche Hσ".
+  rewrite /scheduled in Hsche.
+  simpl in Hsche.
+  rewrite /scheduler in Hsche.
+  apply bool_decide_unpack in Hsche as Hcur.
+  clear Hsche.
   apply fin_to_nat_inj in Hcur.
   iModIntro.
-  iDestruct "Hσ" as "(Htok & Hmem & Hreg & Htx & Hrxagree & Hrxoption & Howned & Haccess & Hrest)".
+  iDestruct "Hσ" as "(Hn & Hmem & Hreg & Hrx & Hown & Hownmb & Haccess & Hres)".
   pose proof (decode_instruction_valid w1 instr Hdecode) as Hvalidinstr.
   inversion Hvalidinstr as [| | | | src dst Hvalidra | | | | | | |] .
   subst src dst.
@@ -32,16 +36,16 @@ Proof.
   (* valid regs *)
   iDestruct ((gen_reg_valid3 i PC ai ra w3 NZ w4 Hcur) with "Hreg Hpc Hra Hnz") as "[%HPC [%Hra %HNZ]]";eauto.
   (* valid pt *)
-  iDestruct ((gen_access_valid_addr_Set ai) with "Haccess Hacc") as %Hacc;eauto.
+  iDestruct (access_agree_check_true (tpa ai) i with "Haccess Hacc") as %Hacc;first set_solver + Hin.
   (* valid mem *)
-  iDestruct (gen_mem_valid ai w1  with "Hmem Hapc") as %Hmem.
+  iDestruct (gen_mem_valid ai w1 with "Hmem Hapc") as %Hmem.
   iSplit.
   - (* reducible *)
     iPureIntro.
     apply (reducible_normal i instr ai w1);eauto.
   - (* step *)
     iModIntro.
-    iIntros (m2 σ2) "%HstepP".
+    iIntros (m2 σ2) "[%P PAuth] %HstepP".
     apply (step_ExecI_normal i instr ai w1 ) in HstepP;eauto.
     remember (exec instr σ1) as c2 eqn:Heqc2.
     rewrite /exec /instr (cmp_word_ExecI σ1 ra w3 w2 HneqPCa HneqNZa Hra) /update_incr_PC /update_reg in Heqc2.
@@ -51,53 +55,201 @@ Proof.
     rewrite_reg_pc.
     rewrite_reg_global.
     rewrite Hcur.
-    iFrame "Htok Htx Hrxagree Hrxoption Howned Hrest".
+    iFrame.
     (* updated part *)
     rewrite -> (update_offset_PC_update_PC1 _ i ai 1);eauto.
     rewrite update_reg_global_update_reg;[|solve_reg_lookup].
     + destruct (w3 <? (of_imm w2))%f,  ((of_imm w2) <? w3)%f.
       iDestruct ((gen_reg_update2_global PC i ai (ai ^+ 1)%f NZ i w4 W2 ) with "Hreg Hpc Hnz") as ">[Hreg [Hpc Hnz]]";eauto.
       iModIntro.
-      iFrame "Hmem Hreg Haccess".
-      iApply "Hϕ".
-      by iFrame "Hapc Hra Hpc Hnz".
+      rewrite /just_scheduled_vms.
+      rewrite /just_scheduled.
+      assert (filter
+                (λ id : vmid,
+                        base.negb (scheduled σ1 id) && scheduled (update_offset_PC (update_reg_global σ1 i NZ W2) 1) id = true)
+                (seq 0 n) = []) as ->.
+      {
+        rewrite /scheduled /machine.scheduler //= /scheduler Hcur.        
+        rewrite update_offset_PC_preserve_current_vm.
+        rewrite update_reg_global_preserve_current_vm.
+        rewrite Hcur.
+        induction n.
+        - simpl.
+          rewrite filter_nil //=.
+        - rewrite seq_S.
+          rewrite filter_app.
+          rewrite IHn.
+          simpl.
+          rewrite filter_cons_False //=.
+          rewrite andb_negb_l.
+          done.
+      }
+      iSimpl.
+      iFrame.
+      iSplitL "PAuth".
+      by iExists P.
+      iSplit; first done.
+      assert ((scheduled (update_offset_PC (update_reg_global σ1 i NZ W2) 1) i) = true) as ->.
+      rewrite /scheduled.
+      simpl.
+      rewrite /scheduler.
+      rewrite update_offset_PC_preserve_current_vm.
+      rewrite update_reg_global_preserve_current_vm.
+      rewrite Hcur.
+      rewrite bool_decide_eq_true.
+      reflexivity.
+      simpl.
+      iApply ("Hϕ" with "[Hapc Hacc Hra Hpc Hnz]").
+      iFrame.
       iDestruct ((gen_reg_update2_global PC i ai (ai ^+ 1)%f NZ i w4 W2 ) with "Hreg Hpc Hnz") as ">[Hreg [Hpc Hnz]]";eauto.
       iModIntro.
-      iFrame "Hmem Hreg Haccess".
-      iApply "Hϕ".
-      by iFrame "Hapc Hra Hpc Hnz".
-      iDestruct ((gen_reg_update2_global PC i ai (ai ^+ 1)%f NZ i w4 W0 ) with "Hreg Hpc Hnz") as ">[Hreg [Hpc Hnz]]";eauto.      iModIntro.
-      iFrame "Hmem Hreg Haccess".
-      iApply "Hϕ".
-      by iFrame "Hapc Hra Hpc Hnz".
+      rewrite /just_scheduled_vms.
+      rewrite /just_scheduled.
+      assert (filter
+                (λ id : vmid,
+                        base.negb (scheduled σ1 id) && scheduled (update_offset_PC (update_reg_global σ1 i NZ W2) 1) id = true)
+                (seq 0 n) = []) as ->.
+      {
+        rewrite /scheduled /machine.scheduler //= /scheduler Hcur.        
+        rewrite update_offset_PC_preserve_current_vm.
+        rewrite update_reg_global_preserve_current_vm.
+        rewrite Hcur.
+        induction n.
+        - simpl.
+          rewrite filter_nil //=.
+        - rewrite seq_S.
+          rewrite filter_app.
+          rewrite IHn.
+          simpl.
+          rewrite filter_cons_False //=.
+          rewrite andb_negb_l.
+          done.
+      }
+      iSimpl.
+      iFrame.
+      iSplitL "PAuth".
+      by iExists P.
+      iSplit; first done.
+      assert ((scheduled (update_offset_PC (update_reg_global σ1 i NZ W2) 1) i) = true) as ->.
+      rewrite /scheduled.
+      simpl.
+      rewrite /scheduler.
+      rewrite update_offset_PC_preserve_current_vm.
+      rewrite update_reg_global_preserve_current_vm.
+      rewrite Hcur.
+      rewrite bool_decide_eq_true.
+      reflexivity.
+      simpl.
+      iApply ("Hϕ" with "[Hapc Hacc Hra Hpc Hnz]").
+      iFrame.
+      iDestruct ((gen_reg_update2_global PC i ai (ai ^+ 1)%f NZ i w4 W0 ) with "Hreg Hpc Hnz") as ">[Hreg [Hpc Hnz]]";eauto.
+      iModIntro.
+      rewrite /just_scheduled_vms.
+      rewrite /just_scheduled.
+      assert (filter
+                (λ id : vmid,
+                        base.negb (scheduled σ1 id) && scheduled (update_offset_PC (update_reg_global σ1 i NZ W0) 1) id = true)
+                (seq 0 n) = []) as ->.
+      {
+        rewrite /scheduled /machine.scheduler //= /scheduler Hcur.        
+        rewrite update_offset_PC_preserve_current_vm.
+        rewrite update_reg_global_preserve_current_vm.
+        rewrite Hcur.
+        induction n.
+        - simpl.
+          rewrite filter_nil //=.
+        - rewrite seq_S.
+          rewrite filter_app.
+          rewrite IHn.
+          simpl.
+          rewrite filter_cons_False //=.
+          rewrite andb_negb_l.
+          done.
+      }
+      iSimpl.
+      iFrame.
+      iSplitL "PAuth".
+      by iExists P.
+      iSplit; first done.
+      assert ((scheduled (update_offset_PC (update_reg_global σ1 i NZ W0) 1) i) = true) as ->.
+      rewrite /scheduled.
+      simpl.
+      rewrite /scheduler.
+      rewrite update_offset_PC_preserve_current_vm.
+      rewrite update_reg_global_preserve_current_vm.
+      rewrite Hcur.
+      rewrite bool_decide_eq_true.
+      reflexivity.
+      simpl.
+      iApply ("Hϕ" with "[Hapc Hacc Hra Hpc Hnz]").
+      iFrame.
       iDestruct ((gen_reg_update2_global PC i ai (ai ^+ 1)%f NZ i w4 W1 ) with "Hreg Hpc Hnz") as ">[Hreg [Hpc Hnz]]";eauto.
       iModIntro.
-      iFrame "Hmem Hreg Haccess".
-      iApply "Hϕ".
-      by iFrame "Hapc Hra Hpc Hnz".
+      rewrite /just_scheduled_vms.
+      rewrite /just_scheduled.
+      assert (filter
+                (λ id : vmid,
+                        base.negb (scheduled σ1 id) && scheduled (update_offset_PC (update_reg_global σ1 i NZ W1) 1) id = true)
+                (seq 0 n) = []) as ->.
+      {
+        rewrite /scheduled /machine.scheduler //= /scheduler Hcur.        
+        rewrite update_offset_PC_preserve_current_vm.
+        rewrite update_reg_global_preserve_current_vm.
+        rewrite Hcur.
+        induction n.
+        - simpl.
+          rewrite filter_nil //=.
+        - rewrite seq_S.
+          rewrite filter_app.
+          rewrite IHn.
+          simpl.
+          rewrite filter_cons_False //=.
+          rewrite andb_negb_l.
+          done.
+      }
+      iSimpl.
+      iFrame.
+      iSplitL "PAuth".
+      by iExists P.
+      iSplit; first done.
+      assert ((scheduled (update_offset_PC (update_reg_global σ1 i NZ W1) 1) i) = true) as ->.
+      rewrite /scheduled.
+      simpl.
+      rewrite /scheduler.
+      rewrite update_offset_PC_preserve_current_vm.
+      rewrite update_reg_global_preserve_current_vm.
+      rewrite Hcur.
+      rewrite bool_decide_eq_true.
+      reflexivity.
+      simpl.
+      iApply ("Hϕ" with "[Hapc Hacc Hra Hpc Hnz]").
+      iFrame.
     + rewrite update_reg_global_update_reg;[|solve_reg_lookup].
       apply (get_reg_gmap_get_reg_Some _ _ _ i) in HPC;eauto.
       by simplify_map_eq /=.
 Qed.
 
-Lemma cmp_reg {i w1 w2 w3 w4 q sacc} ai ra rb :
+Lemma cmp_reg {i w1 w2 w3 w4 q s} ai ra rb :
   decode_instruction w1 = Some(Cmp ra (inr rb)) ->
-  to_pid_aligned ai ∈ sacc ->
+  i ∈ s ->
   {SS{{ ▷ (PC @@ i ->r ai) ∗ ▷ (ai ->a w1) ∗
-        ▷ (ra @@ i ->r w2) ∗ ▷ (rb @@ i ->r w3) ∗
-        ▷ (A@i:={q}[sacc]) ∗ ▷ (NZ @@ i ->r w4)}}}
+        ▷ (ra @@ i ->r w2) ∗ ▷ (rb @@ i ->r w3) ∗ ▷ ((tpa ai) -@{q}A> [s]) ∗ ▷ (NZ @@ i ->r w4)}}}
     ExecI @ i
-    {{{ RET ExecI;  PC @@ i ->r (ai ^+ 1)%f ∗ ai ->a w1 ∗ ra @@ i ->r w2 ∗ rb @@ i ->r w3 ∗
-              A@i:={q}[sacc] ∗ NZ @@ i ->r (if (w2 <? w3)%f then W2 else if (w3 <? w2)%f then W0 else W1) }}}.
+    {{{ RET (false, ExecI); PC @@ i ->r (ai ^+ 1)%f ∗ ai ->a w1 ∗ ra @@ i ->r w2 ∗ (rb @@ i ->r w3) ∗
+        ((tpa ai) -@{q}A> [s]) ∗ NZ @@ i ->r (if (w2 <? w3)%f then W2 else if (w3 <? w2)%f then W0 else W1) }}}.
 Proof.
-  iIntros (Hdecode Hin ϕ) "( >Hpc & >Hapc & >Hra & >Hrb & >Hacc & >Hnz ) Hϕ".
+  iIntros (Hdecode Hin ϕ) "(>Hpc & >Hapc & >Hra & >Hrb & >Hacc & >Hnz ) Hϕ".
   iApply (sswp_lift_atomic_step ExecI);[done|].
-  iIntros (σ1) "%Hsche Hσ".
-  inversion Hsche as [ Hcur ]; clear Hsche.
+  set (instr:= Cmp ra (inr rb)).
+  iIntros (n σ1) "%Hsche Hσ".
+  rewrite /scheduled in Hsche.
+  simpl in Hsche.
+  rewrite /scheduler in Hsche.
+  apply bool_decide_unpack in Hsche as Hcur.
+  clear Hsche.
   apply fin_to_nat_inj in Hcur.
   iModIntro.
-  iDestruct "Hσ" as "(Htok & Hmem & Hreg & Htx & Hrxagree & Hrxoption & Howned & Haccess & Hrest)".
-  set (instr:= Cmp ra (inr rb)).
+  iDestruct "Hσ" as "(Hn & Hmem & Hreg & Hrx & Hown & Hownmb & Haccess & Hres)".
   pose proof (decode_instruction_valid w1 instr Hdecode) as Hvalidinstr.
   inversion Hvalidinstr as [ | | | | | src dst Hvalidra Hvalidrb Hneqrarb | | | | | |] .
   subst src dst.
@@ -106,50 +258,194 @@ Proof.
   (* valid regs *)
   iDestruct ((gen_reg_valid4 i PC ai ra w2 rb w3 NZ w4 Hcur) with "Hreg Hpc Hra Hrb Hnz") as "[%HPC [%Hra [%Hrb %HNZ]]]";auto.
   (* valid pt *)
-  iDestruct ((gen_access_valid_addr_Set ai ) with "Haccess Hacc") as %Hacc;eauto.
+  iDestruct (access_agree_check_true (tpa ai) i with "Haccess Hacc") as %Hacc;first set_solver + Hin.
   (* valid mem *)
-  iDestruct (gen_mem_valid ai w1  with "Hmem Hapc") as %Hmem.
+  iDestruct (gen_mem_valid ai w1 with "Hmem Hapc") as %Hmem.
   iSplit.
   - (* reducible *)
     iPureIntro.
     apply (reducible_normal i instr ai w1);eauto.
   - (* step *)
     iModIntro.
-    iIntros (m2 σ2) "%HstepP".
+    iIntros (m2 σ2) "[%P PAuth] %HstepP".
     apply (step_ExecI_normal i instr ai w1 ) in HstepP;eauto.
     remember (exec instr σ1) as c2 eqn:Heqc2.
-    rewrite /exec /instr (cmp_reg_ExecI σ1 ra w2 rb w3) /update_incr_PC /update_reg in Heqc2;eauto.
+    rewrite /exec /instr (cmp_reg_ExecI σ1 ra w2 rb w3 HneqPCa HneqNZa HneqPCb HneqNZb) /update_incr_PC /update_reg in Heqc2; auto.
     destruct HstepP;subst m2 σ2; subst c2; simpl.
     rewrite /gen_vm_interp.
     (* unchanged part *)
     rewrite_reg_pc.
     rewrite_reg_global.
     rewrite Hcur.
-    iFrame "Htok Htx Hrxagree Hrxoption Howned Hrest".
+    iFrame.
     (* updated part *)
     rewrite -> (update_offset_PC_update_PC1 _ i ai 1);eauto.
     rewrite update_reg_global_update_reg;[|solve_reg_lookup].
     + destruct (w2 <? w3)%f,  (w3 <? w2)%f.
       iDestruct ((gen_reg_update2_global PC i ai (ai ^+ 1)%f NZ i w4 W2 ) with "Hreg Hpc Hnz") as ">[Hreg [Hpc Hnz]]";eauto.
       iModIntro.
-      iFrame "Hmem Hreg Haccess".
-      iApply "Hϕ".
-      by iFrame "Hpc Hapc Hra Hrb Hnz".
+      rewrite /just_scheduled_vms.
+      rewrite /just_scheduled.
+      assert (filter
+                (λ id : vmid,
+                        base.negb (scheduled σ1 id) && scheduled (update_offset_PC (update_reg_global σ1 i NZ W2) 1) id = true)
+                (seq 0 n) = []) as ->.
+      {
+        rewrite /scheduled /machine.scheduler //= /scheduler Hcur.        
+        rewrite update_offset_PC_preserve_current_vm.
+        rewrite update_reg_global_preserve_current_vm.
+        rewrite Hcur.
+        induction n.
+        - simpl.
+          rewrite filter_nil //=.
+        - rewrite seq_S.
+          rewrite filter_app.
+          rewrite IHn.
+          simpl.
+          rewrite filter_cons_False //=.
+          rewrite andb_negb_l.
+          done.
+      }
+      iSimpl.
+      iFrame.
+      iSplitL "PAuth".
+      by iExists P.
+      iSplit; first done.
+      assert ((scheduled (update_offset_PC (update_reg_global σ1 i NZ W2) 1) i) = true) as ->.
+      rewrite /scheduled.
+      simpl.
+      rewrite /scheduler.
+      rewrite update_offset_PC_preserve_current_vm.
+      rewrite update_reg_global_preserve_current_vm.
+      rewrite Hcur.
+      rewrite bool_decide_eq_true.
+      reflexivity.
+      simpl.
+      iApply ("Hϕ" with "[Hapc Hacc Hra Hrb Hpc Hnz]").
+      iFrame.
       iDestruct ((gen_reg_update2_global PC i ai (ai ^+ 1)%f NZ i w4 W2 ) with "Hreg Hpc Hnz") as ">[Hreg [Hpc Hnz]]";eauto.
       iModIntro.
-      iFrame "Hmem Hreg Haccess".
-      iApply "Hϕ".
-      by iFrame "Hpc Hapc Hra Hrb Hnz".
+      rewrite /just_scheduled_vms.
+      rewrite /just_scheduled.
+      assert (filter
+                (λ id : vmid,
+                        base.negb (scheduled σ1 id) && scheduled (update_offset_PC (update_reg_global σ1 i NZ W2) 1) id = true)
+                (seq 0 n) = []) as ->.
+      {
+        rewrite /scheduled /machine.scheduler //= /scheduler Hcur.        
+        rewrite update_offset_PC_preserve_current_vm.
+        rewrite update_reg_global_preserve_current_vm.
+        rewrite Hcur.
+        induction n.
+        - simpl.
+          rewrite filter_nil //=.
+        - rewrite seq_S.
+          rewrite filter_app.
+          rewrite IHn.
+          simpl.
+          rewrite filter_cons_False //=.
+          rewrite andb_negb_l.
+          done.
+      }
+      iSimpl.
+      iFrame.
+      iSplitL "PAuth".
+      by iExists P.
+      iSplit; first done.
+      assert ((scheduled (update_offset_PC (update_reg_global σ1 i NZ W2) 1) i) = true) as ->.
+      rewrite /scheduled.
+      simpl.
+      rewrite /scheduler.
+      rewrite update_offset_PC_preserve_current_vm.
+      rewrite update_reg_global_preserve_current_vm.
+      rewrite Hcur.
+      rewrite bool_decide_eq_true.
+      reflexivity.
+      simpl.
+      iApply ("Hϕ" with "[Hapc Hacc Hra Hrb Hpc Hnz]").
+      iFrame.
       iDestruct ((gen_reg_update2_global PC i ai (ai ^+ 1)%f NZ i w4 W0 ) with "Hreg Hpc Hnz") as ">[Hreg [Hpc Hnz]]";eauto.
       iModIntro.
-      iFrame "Hmem Hreg Haccess".
-      iApply "Hϕ".
-      by iFrame "Hpc Hapc Hra Hrb Hnz".
+      rewrite /just_scheduled_vms.
+      rewrite /just_scheduled.
+      assert (filter
+                (λ id : vmid,
+                        base.negb (scheduled σ1 id) && scheduled (update_offset_PC (update_reg_global σ1 i NZ W0) 1) id = true)
+                (seq 0 n) = []) as ->.
+      {
+        rewrite /scheduled /machine.scheduler //= /scheduler Hcur.        
+        rewrite update_offset_PC_preserve_current_vm.
+        rewrite update_reg_global_preserve_current_vm.
+        rewrite Hcur.
+        induction n.
+        - simpl.
+          rewrite filter_nil //=.
+        - rewrite seq_S.
+          rewrite filter_app.
+          rewrite IHn.
+          simpl.
+          rewrite filter_cons_False //=.
+          rewrite andb_negb_l.
+          done.
+      }
+      iSimpl.
+      iFrame.
+      iSplitL "PAuth".
+      by iExists P.
+      iSplit; first done.
+      assert ((scheduled (update_offset_PC (update_reg_global σ1 i NZ W0) 1) i) = true) as ->.
+      rewrite /scheduled.
+      simpl.
+      rewrite /scheduler.
+      rewrite update_offset_PC_preserve_current_vm.
+      rewrite update_reg_global_preserve_current_vm.
+      rewrite Hcur.
+      rewrite bool_decide_eq_true.
+      reflexivity.
+      simpl.
+      iApply ("Hϕ" with "[Hapc Hacc Hra Hrb Hpc Hnz]").
+      iFrame.
       iDestruct ((gen_reg_update2_global PC i ai (ai ^+ 1)%f NZ i w4 W1 ) with "Hreg Hpc Hnz") as ">[Hreg [Hpc Hnz]]";eauto.
       iModIntro.
-      iFrame "Hmem Hreg Haccess".
-      iApply "Hϕ".
-      by iFrame "Hpc Hapc Hra Hrb Hnz".
+      rewrite /just_scheduled_vms.
+      rewrite /just_scheduled.
+      assert (filter
+                (λ id : vmid,
+                        base.negb (scheduled σ1 id) && scheduled (update_offset_PC (update_reg_global σ1 i NZ W1) 1) id = true)
+                (seq 0 n) = []) as ->.
+      {
+        rewrite /scheduled /machine.scheduler //= /scheduler Hcur.        
+        rewrite update_offset_PC_preserve_current_vm.
+        rewrite update_reg_global_preserve_current_vm.
+        rewrite Hcur.
+        induction n.
+        - simpl.
+          rewrite filter_nil //=.
+        - rewrite seq_S.
+          rewrite filter_app.
+          rewrite IHn.
+          simpl.
+          rewrite filter_cons_False //=.
+          rewrite andb_negb_l.
+          done.
+      }
+      iSimpl.
+      iFrame.
+      iSplitL "PAuth".
+      by iExists P.
+      iSplit; first done.
+      assert ((scheduled (update_offset_PC (update_reg_global σ1 i NZ W1) 1) i) = true) as ->.
+      rewrite /scheduled.
+      simpl.
+      rewrite /scheduler.
+      rewrite update_offset_PC_preserve_current_vm.
+      rewrite update_reg_global_preserve_current_vm.
+      rewrite Hcur.
+      rewrite bool_decide_eq_true.
+      reflexivity.
+      simpl.
+      iApply ("Hϕ" with "[Hapc Hacc Hra Hrb Hpc Hnz]").
+      iFrame.
     + rewrite update_reg_global_update_reg;[|solve_reg_lookup].
       apply (get_reg_gmap_get_reg_Some _ _ _ i) in HPC;eauto.
       by simplify_map_eq /=.
