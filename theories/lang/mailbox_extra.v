7d911d257aecadf3d2d2bc5c581227d3719ba6a5
Zongyuan Liu <zy.liu@cs.au.dk>
2022-03-11 13:37:34 +0100

    rules: msg_poll done

--- theories/lang/mailbox_extra.v
+++ theories/lang/mailbox_extra.v
@@ -8,7 +8,7 @@ Context {HyperConst : HypervisorConstants}.
 Implicit Type σ : state.
 Implicit Type i : VMID.
 
-Lemma empty_rx_preserve_current_vm σ :
+Lemma p_empty_rx_current_vm σ :
   get_current_vm (empty_rx σ) = get_current_vm σ.
 Proof.
   rewrite /empty_rx.
@@ -17,7 +17,7 @@ Proof.
   done.
 Qed.
 
-Lemma empty_rx_preserve_mem σ:
+Lemma p_empty_rx_mem σ:
   get_mem (empty_rx σ) = get_mem σ.
 Proof.
   rewrite /empty_rx.
@@ -26,8 +26,8 @@ Proof.
   done.
 Qed.
 
-Lemma empty_rx_preserve_regs σ:
- get_reg_gmap (empty_rx σ) = get_reg_gmap σ.
+Lemma p_empty_rx_regs σ:
+ get_reg_files (empty_rx σ) = get_reg_files σ.
 Proof.
   rewrite /empty_rx.
   destruct (σ.1.1.1.1.2 !!! σ.1.1.2).
@@ -35,14 +35,129 @@ Proof.
   done.
 Qed.
 
-Lemma empty_rx_preserve_mb σ:
+Lemma p_empty_rx_mb σ:
   get_mb_gmap (empty_rx σ) = (get_mb_gmap σ).
 Proof.
   rewrite /empty_rx.
-(* XXX: couldn't find useful lemmas about flat_map, maybe redefine get_mb_gmap? *)
-Admitted.
+  destruct (σ.1.1.1.1.2 !!! σ.1.1.2) eqn:Heqn_mb.
+  destruct p.
+  rewrite /get_mb_gmap /=.
+  set i := (σ.1.1.2).
+  apply map_eq.
+  intros [j RT].
+  destruct (list_to_map (flat_map (λ v : VMID, [(v, TX, (σ.1.1.1.1.2 !!! v).1); (v, RX, (σ.1.1.1.1.2 !!! v).2.1)]) list_of_vmids) !! (j, RT)) eqn:Heqn.
+  rewrite -elem_of_list_to_map'.
+  rewrite elem_of_list_In.
+  rewrite in_flat_map.
+  exists j.
+  split. apply in_list_of_vmids.
+  apply elem_of_list_to_map_2 in Heqn.
+  rewrite elem_of_list_In in Heqn.
+  rewrite in_flat_map in Heqn.
+  rewrite -elem_of_list_In.
+  destruct Heqn as [? [_ ?]].
+  apply in_inv in H.
+  destruct H.
+  inversion H.
+  subst x RT.
+  assert (p0 = (vinsert i (t, (p, None)) σ.1.1.1.1.2 !!! j).1).
+  {
+    destruct (decide (i = j)).
+    subst j.
+    rewrite vlookup_insert /=.
+    rewrite Heqn_mb in H3.
+    simpl in H3.
+    done.
+    rewrite vlookup_insert_ne;last done.
+    done.
+  }
+  rewrite -H0 H.
+  apply elem_of_list_here.
+  apply elem_of_list_further.
+  apply in_inv in H.
+  destruct H.
+  2:{
+    apply in_nil in H.
+    done.
+  }
+  inversion H.
+  subst x RT.
+  assert (p0 = (vinsert i (t, (p, None)) σ.1.1.1.1.2 !!! j).2.1).
+  {
+    simpl in H3.
+    destruct (decide (i = j)).
+    subst j.
+    rewrite vlookup_insert /=.
+    rewrite Heqn_mb in H.
+    inversion H.
+    done.
+    rewrite vlookup_insert_ne;last done.
+    done.
+  }
+  rewrite H H0.
+  apply elem_of_list_here.
+  {
+    intros.
+    rewrite elem_of_list_In in H.
+    rewrite elem_of_list_In in H0.
+    rewrite in_flat_map in H.
+    rewrite in_flat_map in H0.
+    destruct H as [? [_ ?]].
+    destruct H0 as [? [_ ?]].
+    apply in_inv in H.
+    destruct H.
+    inversion H.
+    subst x RT.
+    apply in_inv in H0.
+    destruct H0.
+    2: {
+      apply in_inv in H0.
+      destruct H0;last done.
+      inversion H0.
+    }
+    inversion H0.
+    done.
+    apply in_inv in H.
+    destruct H;last done.
+    inversion H.
+    subst x RT.
+    apply in_inv in H0.
+    destruct H0.
+    inversion H0.
+    apply in_inv in H0.
+    destruct H0;last done.
+    inversion H0.
+    done.
+  }
+  apply not_elem_of_list_to_map in Heqn.
+  exfalso.
+  apply Heqn.
+  rewrite elem_of_list_fmap.
+  destruct RT.
+  {
+    exists (j,RX, (σ.1.1.1.1.2 !!! j).2.1).
+    split;first done.
+    rewrite elem_of_list_In.
+    rewrite in_flat_map.
+    exists j.
+    split;first apply in_list_of_vmids.
+    rewrite -elem_of_list_In.
+    apply elem_of_list_further.
+    apply elem_of_list_here.
+  }
+  {
+    exists (j,TX, (σ.1.1.1.1.2 !!! j).1).
+    split;first done.
+    rewrite elem_of_list_In.
+    rewrite in_flat_map.
+    exists j.
+    split;first apply in_list_of_vmids.
+    rewrite -elem_of_list_In.
+    apply elem_of_list_here.
+  }
+Qed.
 
-Lemma empty_rx_preserve_pagetable σ:
+Lemma p_empty_rx_pgt σ:
   get_page_table (empty_rx σ)  = get_page_table σ.
 Proof.
   rewrite /empty_rx.
@@ -51,7 +166,7 @@ Proof.
   done.
 Qed.
 
-Lemma empty_rx_preserve_trans σ :
+Lemma p_empty_rx_trans σ :
   get_transactions (empty_rx σ) = (get_transactions σ).
 Proof.
   rewrite /empty_rx.
@@ -60,8 +175,7 @@ Proof.
   done.
 Qed.
 
-(* TODO simplify *)
-Lemma empty_rx_update_mailbox σ:
+Lemma u_empty_rx_mb σ:
   get_rx_gmap (empty_rx σ) = <[(get_current_vm σ) := None]>(get_rx_gmap σ).
 Proof.
   rewrite /get_rx_gmap.
